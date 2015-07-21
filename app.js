dfede118667d11bae0f5195bc6e657dc43875b49
cghil <cghilarducci@gmail.com>
2015-07-20 17:01:14 -0700

    added form

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var flash = require('connect-flash');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var posts = require('./routes/posts');
 
 var app = express();
 
@@ -74,6 +75,7 @@ app.use(function(req,res,next){
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/posts', posts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
