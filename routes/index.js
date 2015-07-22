bed131158c26fc3f8c0eed5b601954684e3df33e
cghil <cghilarducci@gmail.com>
2015-07-21 17:10:53 -0700

    fixed error with middleware

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,13 @@ var db = require('monk')('localhost/nodeblog');
 
 // Homepage Blog Posts
 router.get('/', function(req, res, next) {
-	var db = req.db;
-	var posts = db.get('posts');
-	// comes from the callback of find
-	posts.find({}, {}, function(err, posts){
-		res.render('index', {
-			"posts": posts
-		});
-	});
+  var db = req.db;
+  var posts = db.get('posts');
+  posts.find({},{},function(err, posts){
+  	res.render('index',{
+  		"posts": posts
+  	});
+  });
 });
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -4,59 +4,64 @@ var mongo = require('mongodb');
 var db = require('monk')('localhost/nodeblog');
 
 router.get('/add', function(req, res, next){
-	res.render('addpost', {
-		'title': 'Add Post'
+	var categories = db.get('categories');
+
+	categories.find({},{}, function(err, categories){
+		res.render('addpost',{
+			"title": "Add Post",
+			"categories": categories
+		});
 	});
 });
 
 router.post('/add', function(req, res, next){
-	// Get form values
-	var title = req.body.title;
-	var category = req.body.category;
-	var body = req.body.body;
-	var author = req.body.author;
-	var date = new Date();
+	// Get Form Values
+	var title 		= req.body.title;
+	var category 	= req.body.category;
+	var body 		= req.body.body;
+	var author 		= req.body.author;
+	var date 		= new Date();
 
-	if(req.files.mainimage) {
-		// comes from the form
-		var mainImageOriginalName = req.files.mainimage.originalname;
-		var mainImageName = req.files.mainimage.name;
-		var mainImageMime = req.files.mainimage.mimetype;
-		var mainImagePath = req.files.mainimage.path;
-		var mainImageExt = req.files.mainimage.extension;
-		var mainImageSize = req.files.mainimage.size;
+	if(req.files.mainimage){
+		var mainImageOriginalName 	= req.files.mainimage.originalname;
+		var mainImageName 			= req.files.mainimage.name;
+		var mainImageMime    		= req.files.mainimage.mimetype;
+		var mainImagePath    		= req.files.mainimage.path;
+		var mainImageExt    		= req.files.mainimage.extension;
+		var mainImageSize    		= req.files.mainimage.size;
 	} else {
 		var mainImageName = 'noimage.png';
 	}
 
 	// Form Validation
-	req.checkBody('title', "Title field is required").notEmpty();
-	req.checkBody('boby', 'Content is required for post').notEmpty();
+	req.checkBody('title','Title field is required').notEmpty();
+	req.checkBody('body', 'Body field is required');
 
-	// Check errors
+	// Check Errors
 	var errors = req.validationErrors();
 
 	if(errors){
-		res.render('addpost', {
+		res.render('addpost',{
 			"errors": errors,
-			"title" : title,
-			"body" : body
+			"title": title,
+			"body": body
 		});
 	} else {
 		var posts = db.get('posts');
-		//submit to the db
+
+		// Submit to DB
 		posts.insert({
-			"title" : title,
-			"body" : body,
-			"category" : category,
+			"title": title,
+			"body": body,
+			"category": category,
 			"date": date,
-			"author" : author,
-			"mainimage": mainimage
+			"author": author,
+			"mainimage": mainImageName
 		}, function(err, post){
-			if (err){
-				res.send({'There was an issue submitting post'});
+			if(err){
+				res.send('There was an issue submitting the post');
 			} else {
-				req.flash('success', 'Post Submitted');
+				req.flash('success','Post Submitted');
 				res.location('/');
 				res.redirect('/');
 			}
@@ -64,4 +69,4 @@ router.post('/add', function(req, res, next){
 	}
 });
 
-module.exports = router
+module.exports = router;
